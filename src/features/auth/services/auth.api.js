7a7dd85c87f7cffeb91f67b39424f018e47dcdf1
itxlofyy51 <sahernawaz09@gmail.com>
2026-04-03 17:05:18 +0500

    Update auth.api.js

--- src/features/auth/services/auth.api.js
+++ src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { Form } from "react-router";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api/auth",
+  baseURL: import.meta.env.VITE_API_URL/api/auth || "http://localhost:3000/api/auth",
   withCredentials: true, // send cookies automatically
 });
 
