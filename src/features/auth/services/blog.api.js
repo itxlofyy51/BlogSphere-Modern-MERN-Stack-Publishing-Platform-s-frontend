74e6a5f59ebdfca08400b4e935c4233528f1a0c5
itxlofyy51 <sahernawaz09@gmail.com>
2026-04-03 17:08:38 +0500

    Update blog.api.js

--- src/features/auth/services/blog.api.js
+++ src/features/auth/services/blog.api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL/api/blog || "http://localhost:3000/api/blog",
+  import.meta.env.VITE_API_URL 
+        ? `${import.meta.env.VITE_API_URL}/api/auth` 
+        : "http://localhost:3000/api/auth",
   withCredentials: true,
 });
 
